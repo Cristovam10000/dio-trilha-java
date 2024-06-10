4ef3f4b797871dcff7bdd57afb21bad39951a454
cristovam <cristovam1712@gmail.com>
2024-06-10 15:40:50 -0300

    um exemplo simples

--- tipos_variaveis/src/TiposVariaveis.java
+++ tipos_variaveis/src/TiposVariaveis.java
@@ -1,5 +1,11 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        double salarioMinimo = 2.500;
+        float pi = 3.14F;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
     }
 }
